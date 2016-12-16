ec15b849a2bd105ff8066bc7799571bf5e5cd457
Eagle Brosi <evbrosi@gmail.com>
2016-12-16 14:23:34 -0500

    ffaker employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,15 @@ organization = Organization.create(
                                togglit_id: "TEKY"
 )
 
-employees = Employee.create(
+employees = 100.times do
+            Employee.create(
                        first_name: FFaker::Name.first_name,
                        last_name: FFaker::Name.last_name,
                        title: FFaker::Job.title,
                        email: FFaker::Internet.email,
                        organization_id: 1
 )
+end
 
 location = Location.create(
                         title: "Paintsville",
